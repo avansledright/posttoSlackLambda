b7b05f4b6821b56b30acc68c7368262067a60036
VanSledright, Aaron J <aaron.j.vansledright@accenture.com>
2020-08-27 10:13:23 -0500

    Adding in file name into the response

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,26 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
-
+import requests
+import boto3
 import os
+import json
+from urllib.parse import unquote_plus
 from slack import WebClient
 from slack.errors import SlackApiError
 
-def lambda_handler():
-    slack_token = os.environ["slackBot"]
-    client = WebClient(token=slack_token)    
-    try:
-        response = client.chat_postMessage(
-            channel="####################",
-            text="A file has been uploaded"
-        )
+def lambda_handler(event, lambda_context):
 
+    slack_token = os.environ["slackBot"]
+    client = WebClient(token=slack_token)
+    
+    try:
+        for record in event['Records']:
+            key = unquote_plus(record['s3']['object']['key'])
+            print(key)
+            response = client.chat_postMessage(
+                channel="##################",  # Replace this with your Channel ID
+                text="A File was uploaded",    # This is the notification text
+                blocks = [{"type": "section", "text": {"type": "plain_text", "text": key}}]
+        )
     except SlackApiError as e:
         assert e.response["error"]
-    return 
