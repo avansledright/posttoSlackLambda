803cc7a3dafd61614dda84f2e082b1e4eab48a06
VanSledright, Aaron J <aaron.j.vansledright@accenture.com>
2020-08-27 10:55:59 -0500

    Tweaked message posted to slack

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,6 @@ logging.basicConfig(level=logging.DEBUG)
 import requests
 import boto3
 import os
-import json
 from urllib.parse import unquote_plus
 from slack import WebClient
 from slack.errors import SlackApiError
@@ -16,11 +15,11 @@ def lambda_handler(event, lambda_context):
     try:
         for record in event['Records']:
             key = unquote_plus(record['s3']['object']['key'])
-            print(key)
+            response_string = f"The file: {key} has been uploaded"
             response = client.chat_postMessage(
                 channel="##################",  # Replace this with your Channel ID
                 text="A File was uploaded",    # This is the notification text
-                blocks = [{"type": "section", "text": {"type": "plain_text", "text": key}}]
+                blocks = [{"type": "section", "text": {"type": "plain_text", "text": response_string }}]
         )
     except SlackApiError as e:
         assert e.response["error"]
